7ddb46da1520788d9201278c77277119d5891c56
Jarrett Colby <jarrettcolby@gmail.com>
2015-04-17 17:36:33 -0400

    Change environment variable name

--- put.sh
+++ put.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ssh minecraft@$CREEPERHOST_SERVER_HOST "mkdir -p /home/minecraft/world/computer/$1/ && mkdir -p /home/minecraft/world/computer/$2/ && echo \"shell.run('harvest')\" > /home/minecraft/world/computer/$1/startup && echo \"shell.run('plant')\" > /home/minecraft/world/computer/$2/startup"
-scp harvest.lua minecraft@$CREEPERHOST_SERVER_HOST:/home/minecraft/world/computer/$1/harvest
-#scp plant.lua minecraft@$CREEPERHOST_SERVER_HOST:/home/minecraft/world/computer/$2/plant
+ssh minecraft@$FTB_SERVER_HOST "mkdir -p /home/minecraft/world/computer/$1/ && mkdir -p /home/minecraft/world/computer/$2/ && echo \"shell.run('harvest')\" > /home/minecraft/world/computer/$1/startup && echo \"shell.run('plant')\" > /home/minecraft/world/computer/$2/startup"
+scp harvest.lua minecraft@$FTB_SERVER_HOST:/home/minecraft/world/computer/$1/harvest
+#scp plant.lua minecraft@$FTB_SERVER_HOST:/home/minecraft/world/computer/$2/plant
